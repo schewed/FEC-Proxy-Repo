1e3b65d766229dba8075af30054a15a9663ef3e7
Daniel Schewe <danielschewe@Daniels-MacBook-Pro.local>
2019-07-23 14:32:00 -0600

    proxy update, simplified

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8888;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+// comment out the below line when setting up the proxy
+// need .env for this file and express.static for the proxy
+app.use(express.static(__dirname + '/../public/client/dist'));
+
+// middleware as necessary
+
+app.get('/', function (req, res, next) {
+    console.log('test successful!');
+    res.end();
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
